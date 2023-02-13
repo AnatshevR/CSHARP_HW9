00270f79dd7d466868c9401d54c23e9c49fc0b58
Roman_Anatshev <romananatshev@gmail.com>
2023-02-13 11:42:06 +0300

    Показать натуральные числа от M до N, N и M заданы

---
+++ Task2_FromMtoN/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void showMe(int M, int N)
+{
+    if(M==N) return;
+    if (M>N)
+    {
+        Console.Write(M + " ");
+        showMe (M-1,N);
+    }
+    else
+    {
+        Console.Write(M + " ");
+        showMe(M+1,N);
+    }
+}
+
+
+Console.WriteLine("Enter integer M");
+int M= int.Parse(Console.ReadLine());
+Console.WriteLine("Enter integer N");
+int N= int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+showMe (M,N);
+Console.WriteLine(N);
