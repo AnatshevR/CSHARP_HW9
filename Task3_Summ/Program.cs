db97f0efb46e1174ac2abeae4fc9f56b1cc8be4c
Roman_Anatshev <romananatshev@gmail.com>
2023-02-13 11:58:08 +0300

    Найти сумму элементов от M до N, N и M заданы

---
+++ Task3_Summ/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int Summ (int M, int N)
+{
+    if (M==N) return M;
+    if (M>N)
+    {
+        return M + Summ(M-1,N);
+    }
+    else
+    {
+        return M + Summ(M+1,N);
+    }
+}
+
+
+Console.WriteLine("Enter integer M");
+int M= int.Parse(Console.ReadLine());
+Console.WriteLine("Enter integer N");
+int N= int.Parse(Console.ReadLine());
+Console.Write("sum of numbers from " + M + " to " + N + " = ");
+Console.WriteLine(Summ(M,N));
